c3f4ab401ef5b608e2fddd16550cb9fa87153277
José Moraes <joseluizjardim@icloud.com>
2025-08-26 13:56:10 -0300

    ajuste no footer redes sociais

--- src/app/precatorio/page.tsx
+++ src/app/precatorio/page.tsx
@@ -1,8 +1,17 @@
 export default function PrecatorioPage() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-2xl font-bold mb-4">Página de Precatórios</h1>
-      <p className="text-lg">Conteúdo relacionado a precatórios será adicionado em breve.</p>
+    <main
+      style={{ minHeight: "calc(100vh - 68px - 148px)" }}
+      className="bg-red-900 py-12"
+    >
+      <div className="container mx-auto flex flex-col gap-12 text-white">
+      <h1 className="text-2xl font-bold mb-4">PRECATÓRIOS</h1>
+      <p className="text-lg">Abaixo serão inseridas as imagens referentes aos precatórios</p>
+      <img className="flex flex-col" src="../public/precatorio/precatorios1.jpg" alt="Precatórios" />
+      <img className="flex flex-col" src="../public/precatorio/precatorios2.jpg" alt="Precatórios" />
+      <img className="flex flex-col" src="../public/precatorio/precatorios3.jpg" alt="Precatórios" />
+
     </div>
+    </main>
   );
 }
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,6 +2,7 @@
 import { FaFacebook } from 'react-icons/fa';
 import { FaInstagram } from 'react-icons/fa';
 import { FaXTwitter } from 'react-icons/fa6';
+import { FaWhatsapp } from 'react-icons/fa';
 
 export default function Footer() {
   return (
@@ -11,8 +12,8 @@ export default function Footer() {
 
         <div className='flex gap-4 w-full'>
           <FaFacebook className='text-white text-xl cursor-pointer' onClick={() => window.open('https://www.facebook.com/SIMCAoficial', '_blank')} />
-          <FaInstagram className='text-white text-xl cursor-pointer' onClick={() => window.open('https://www.instagram.com/simca_oficial/', '_blank')} />
-          <FaXTwitter className='text-white text-xl cursor-pointer' />
+          <FaInstagram className='text-white text-xl cursor-pointer' onClick={() => window.open('https://www.instagram.com/simcacachoeirinha/', '_blank')} />
+          <FaWhatsapp className='text-white text-xl cursor-pointer' onClick={() => window.open('https://wa.me/+555198660241', '_blank')} />
         </div>
 
         <h3 className='text-white text-xl font-bold w-1/3 text-center'>
