78a821eee6ef62d14d63c88c6037612ba398daa5
José Moraes <joseluizjardim@icloud.com>
2025-11-03 03:23:26 -0300

    ueba

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    allowDevelopmentBuild: ['https://municipariosdecachoeirinha.com.br'],
-  },
   images: {
     remotePatterns: [
       {
